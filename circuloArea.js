b3d84315bdaf512cd4a41a919b4a43eab342594a
Jonathan López Ayala <112882084+john3412@users.noreply.github.com>
2023-06-28 00:22:50 -0600

    Create circuloArea.js

---
+++ circuloArea.js
@@ -0,0 +1,8 @@
+function areaCirculo(radio) { // Función que almacena los metodos y los atributos
+    return (3.141592 * (radio*radio)); // retorna un valor
+}
+
+const radio = 5; 
+const area = areaCirculo(radio);
+
+console.log(`El área del circulo es: ${area}`);
